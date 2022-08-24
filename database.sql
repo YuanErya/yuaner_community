eaf87a891544ffd854faeaef8eef0ae4fe067b30
Lin Tang <1508014225@qq.com>
2022-08-24 22:20:28 +0800

    修改数据库表

--- database.sql
+++ database.sql
@@ -20,7 +20,6 @@ CREATE TABLE `ye_user`  (
                             `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '密码',
                             `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
                             `score` int NOT NULL DEFAULT 0 COMMENT '积分',
-                            `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT 'token',
                             `introduction` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '个人简介',
                             `active` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否激活，1：是，0：否',
                             `status` bit(1) NULL DEFAULT b'1' COMMENT '状态，1：使用，0：停用',
@@ -34,5 +33,5 @@ CREATE TABLE `ye_user`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = DYNAMIC;
 
 
-INSERT INTO `ye_user` VALUES ('1349290158897311745', 'admin', 'admin', '123456',  '23456@qq.com', 2, '','我是超级管理员哦！！', b'1', b'1', NULL, '2021-01-13 17:40:17', NULL);
-INSERT INTO `ye_user` VALUES ('1349618748226658305', 'zhangsan', 'zhangsan', '12345', '123456@qq.com', 0, '', '哥没有简介', b'1', b'1', NULL, '2021-01-14 15:25:59', NULL);
+INSERT INTO `ye_user` VALUES ('1349290158897311745', 'admin', 'admin', '123456',  '23456@qq.com', 2,'我是超级管理员哦！！', b'1', b'1', NULL, '2021-01-13 17:40:17', NULL);
+INSERT INTO `ye_user` VALUES ('1349618748226658305', 'zhangsan', 'zhangsan', '12345', '123456@qq.com', 0,'哥没有简介', b'1', b'1', NULL, '2021-01-14 15:25:59', NULL);
